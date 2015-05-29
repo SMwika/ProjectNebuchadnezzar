5c3fb36a2d74953c7da00a3b3928c20acc5ddd8a
Jakub Pałac <jakub.palac@gmail.com>
2015-05-29 10:56:57 +0200

    added blank WCF interface

---
+++ ServerService/ServerConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerService
+{
+    class ServerConnector : SharedClasses.IServerConnector
+    {
+        public override List<SharedClasses.PacketDB> GetUniqueFileNames()
+        {
+            return new DBConnect().GetUniqueFileNames();
+        }
+
+        public override List<SharedClasses.PacketDB> GetUniqueFileNames(string dt)
+        {
+            return new DBConnect().GetUniqueFileNames(dt);
+        }
+    }
+}
---
+++ SharedClasses/IServerConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace SharedClasses
+{
+    [ServiceContract]
+    public interface IServerConnector
+    {
+        [OperationContract]
+        List<PacketDB> GetUniqueFileNames();
+
+        [OperationContract]
+        List<PacketDB> GetUniqueFileNames(string dt);
+    }
+}
